f626a21317e70737d80508fe820ebf035c48b025
shamique202 <shamique.e@gmail.com>
2022-06-24 22:27:02 -0400

    finished code

--- src/components/App.js
+++ src/components/App.js
@@ -19,15 +19,14 @@ const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header />
-      
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" render={ () => <About title='About' /> } />
-        <Route exact path="/teachers" component={Teachers} />
-        <Route path="/teachers/:topic/:name" component={Featured} />
-        <Route path="/courses" component={Courses} />
-        <Route component={NotFound} />
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" render={ () => <About title='About' /> } />
+          <Route exact path="/teachers" component={Teachers} />
+          <Route path="/teachers/:topic/:name" component={Featured} />
+          <Route path="/courses" component={Courses} />
+          <Route component={NotFound} />
+        </Switch>
     </div>
   </BrowserRouter>
 );
